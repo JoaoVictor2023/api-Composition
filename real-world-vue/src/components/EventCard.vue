c6caf22f679af2f0973f1fef2524c07c1110e86b
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-04-08 14:25:29 +0200

    feat: Retour à EventCard

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,13 +1,35 @@
 <script setup>
-
+import { ref } from 'vue'
+const event = ref({
+    id: 5928101,
+    category: 'animal welfare',
+    title: 'Cat Adoption Day',
+    description: 'Find ypur new feline friend at this event',
+    location: 'Meow Town',
+    date: 'January 28, 2022',
+    time: '12:00',
+    petsAllowed: true,
+    organizer: 'Kat Laydee',
+})
 </script>
 
 <template>
-    <div class="greetings">
-
+    <div class="event-card">
+        <h2>{{ event.title }}</h2>
+        <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
 </template>
 
-<style scoped>
-
+<style scoped>/* scoped permet d'éteindre et d'isoler ces styles à ce seul composant */
+.event-card {
+    padding: 20px;
+    width: 250px;
+    cursor: pointer;
+    border: 1px solid #39455c;
+    margin-bottom: 18px;
+}
+.event-card:hover {
+    transform: scale(1.01);
+    box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
 </style>
