01abf989909843d338828167dc059840595ab7cd
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-05-13 15:48:18 +0200

    feat: 4.7 Encore une optimisation

--- src/views/event/Layout.vue
+++ src/views/event/Layout.vue
@@ -19,15 +19,15 @@ onMounted(() => {
   <div v-if="event">
     <h1>{{ event.title }}</h1>
     <div id="nav">
-      <router-link :to="{name: 'EventDetails', params: { id } }">
+      <router-link :to="{name: 'EventDetails'}">
         Details
       </router-link>
       |
-      <router-link :to="{name: 'EventRegister', params: { id } }">
+      <router-link :to="{name: 'EventRegister'}">
         Register
       </router-link>
       |
-      <router-link :to="{name: 'EventEdit', params: { id } }">
+      <router-link :to="{name: 'EventEdit'}">
         Edit
       </router-link>
     </div>
