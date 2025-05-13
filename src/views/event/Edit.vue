55ee6361944034501436aba4a02fe36d14a3718b
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2025-05-13 15:46:58 +0200

    feat: 4.6 Solution: routes imbriquées

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,36 +1,6 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-import EventService from '@/services/EventService.js'
-
-const { id } = defineProps(['id'])
-
-const event = ref(null)
-onMounted(() => {
-  EventService.getEvent(id)
-  .then(response => {
-    event.value = response.data
-  })
-  .catch(error => {
-    console.log(error);
-  })
-})
+defineProps(['event'])
 </script>
 <template>
-  <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <div id="nav">
-      <router-link :to="{name: 'EventDetails', params: { id } }">
-        Details
-      </router-link>
-      |
-      <router-link :to="{name: 'EventRegister', params: { id } }">
-        Register
-      </router-link>
-      |
-      <router-link :to="{name: 'EventEdit', params: { id } }">
-        Edit
-      </router-link>
-    </div>
-      <p>Edit the event here</p>
-  </div>
+  <p>Edit the event here</p>
 </template>
